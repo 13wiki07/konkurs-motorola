942a9e879ec5e4764d783288f597e6edfd69c5cc
leman789 <leman3211@wp.pl>
2022-12-06 21:27:52 +0100

    Generate_HitBox

--- ArkanoEgo/GamePage.xaml.cs
+++ ArkanoEgo/GamePage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
@@ -23,7 +24,7 @@ namespace ArkanoEgo
         public GamePage()
         {
             InitializeComponent();
-
+            GenerateElements();
             myCanvas.Focus();
 
             gameTimer.Interval = TimeSpan.FromMilliseconds(20);
@@ -31,8 +32,48 @@ namespace ArkanoEgo
             gameTimer.Start();
 
         }
+        public void GenerateElements()
+        {
+            int top = 0;
+            int left = 0;
+            for (int i = 0; i < 12; i++){//x
+
+                for (int j = 0; j < 12; j++)//y
+                {
+                    // Create the rectangle
+                    Rectangle rec = new Rectangle()
+                    {
+                        Width = 80,
+                        Height = 47,
+                        Fill = Brushes.Green,
+                        Stroke = Brushes.Red,
+                        StrokeThickness = 1,
+                        Name = "Element" + i.ToString() + j.ToString()
+                    };
+
+                    // Add to a canvas for example
+                    myCanvas.Children.Add(rec);
+                    Canvas.SetTop(rec, top);
+                    Canvas.SetLeft(rec, left);
+                    top = top + 47;
+                }
+                left = left + 80;
+                top = 0;
+            }
+        }
+
         private void GameTimerEvent(object sender, EventArgs e)
         {
+            foreach(var x in myCanvas.Children.OfType<Rectangle>())
+            {
+                Rect ballHitBox = new Rect(Canvas.GetLeft(ball),Canvas.GetTop(ball),ball.Width,ball.Height);
+                Rect BlockHitBox = new Rect(Canvas.GetLeft(x),Canvas.GetTop(x),x.Width,x.Height);
+                if (ballHitBox.IntersectsWith(BlockHitBox)){
+                    myCanvas.Children.Remove(x);
+                    break;
+                }
+            }
+
             if (goDown)
             {
                 Canvas.SetTop(ball, Canvas.GetTop(ball) + 10);
