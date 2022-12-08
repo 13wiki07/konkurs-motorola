224c70d859392e2a343e16795cd3c9e4613664b1
leman789 <leman3211@wp.pl>
2022-12-08 22:43:52 +0100

    BrickClasses

---
+++ ArkanoEgo/Classes/Bricks/Brick.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkanoEgo.Classes.Bricks
+{
+    public abstract class Brick
+    {
+        private string _color;
+        private bool _canBreak;
+
+        public Brick(string color, bool canBreak)
+        {
+            _color = color;
+            _canBreak = canBreak;
+        }
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }
+
+        public bool CanBreak
+        {
+            get 
+            { 
+                return _canBreak; 
+            }
+            set
+            {
+                _canBreak = value;
+            }
+        }
+    }
+}
---
+++ ArkanoEgo/Classes/Bricks/SimpleBrick.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkanoEgo.Classes.Bricks
+{
+    public class SimpleBrick : Brick
+    {
+        private int _value;
+        private int _timesToBreak;
+        public SimpleBrick(string color, int value) : base(color, true)
+        {
+            _value = value;
+            _timesToBreak = 1;
+        }
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+        public int TimesToBreak
+        {
+            get
+            {
+                return _timesToBreak;
+            }
+            set
+            {
+                _timesToBreak = value;
+            }
+        }
+    }
+}
